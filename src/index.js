bf98ea4cace71b59e0bd6b1b718c6ed760105c12
Waqas Noor <waqas2c@yahoo.com>
2020-07-18 16:39:22 +0500

    Animating loading thread

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 window.addEventListener("load", (event) => {
-  //   const body = document.getElementsByTagName("body")[0];
-  //   body.classList.add("playing");
+  const body = document.getElementsByTagName("body")[0];
+  const ball = document.getElementsByClassName("ball")[0];
+  ball.addEventListener("animationend", () => {
+    body.className = "";
+  });
+  body.classList.add("playing");
 });
