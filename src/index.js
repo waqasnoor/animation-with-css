cc1fa822f935165f9dc6f4ee8da375453a24983f
Waqas Noor <waqas2c@yahoo.com>
2020-07-18 17:43:00 +0500

    making LoadingGif

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
+let scene;
+let body;
+let transitionEnd = "hidden";
 window.addEventListener("load", (event) => {
-  const body = document.getElementsByTagName("body")[0];
+  body = document.getElementsByTagName("body")[0];
   const ball = document.getElementsByClassName("ball")[0];
   ball.addEventListener("animationend", () => {
-    body.className = "";
+    toggleScene();
   });
-  body.classList.add("playing");
+  const loading = document.getElementsByClassName("loading")[0];
+  loading.addEventListener("transitionend", () => {
+    // console.log("transitionend");
+    if (transitionEnd === "hidden") {
+      transitionEnd = "display";
+      toggleScene();
+    } else {
+      transitionEnd = "hidden";
+    }
+  });
+  toggleScene();
 });
+
+function toggleScene() {
+  if (scene === "scene1") {
+    scene = "scene2";
+  } else {
+    scene = "scene1";
+  }
+  body.className = scene;
+}
