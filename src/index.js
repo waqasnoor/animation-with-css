7892fbcf0e3a92c992b11ee0adeed6f9d32d0f3d
Waqas Noor <waqas2c@yahoo.com>
2020-07-19 21:59:50 +0500

    loading with event

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 let scene;
-let body;
+let loader;
 let transitionEnd = "hidden";
+let isLoading = true;
 window.addEventListener("load", (event) => {
-  body = document.getElementsByTagName("body")[0];
+  loader = document.getElementsByClassName("loader")[0];
   const ball = document.getElementsByClassName("ball")[0];
   ball.addEventListener("animationend", () => {
     toggleScene();
@@ -21,10 +22,25 @@ window.addEventListener("load", (event) => {
 });
 
 function toggleScene() {
+  // return;
+  // return (container.className = "scene1");
+  if (!isLoading) {
+    loader.className = "loader hidden";
+    return showData();
+  }
   if (scene === "scene1") {
     scene = "scene2";
   } else {
     scene = "scene1";
   }
-  body.className = scene;
+  loader.className = `loader playing ${scene}`;
+}
+setTimeout(() => {
+  isLoading = false;
+}, 6000);
+function showData() {
+  const container = document.getElementsByClassName("container")[0];
+  console.log("show data");
+  console.log({ container });
+  container.className = "container showdata";
 }
